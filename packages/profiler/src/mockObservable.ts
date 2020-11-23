6ce67b9830ef69226570a893f44bb2c7dad6595f
jesse23 <pengwenjia@gmail.com>
2020-11-23 07:08:20 +0000

    feat(tti): naive tti obsserver
    affects: @few/profiler
    
    naive tti observer with test

--- packages/profiler/src/mockObservable.ts
+++ packages/profiler/src/mockObservable.ts
@@ -1,4 +1,5 @@
-import { Observable, Observer } from './types';
+import { Observable, Observer } from '@/types';
+import { now } from '@/profiler';
 interface MockObservable extends Observable {
     mockStart: () => void;
     mockDone:  () => void;
@@ -42,6 +43,7 @@ export const createMockObservable = (): MockObservable => {
 
 interface MockObserver extends Observer {
     getMetrics: () => number;
+    reset: () => void;
 }
 
 export const createMockObserver = (): MockObserver => {
@@ -53,6 +55,34 @@ export const createMockObserver = (): MockObserver => {
         },
         getMetrics: (): number => {
             return _res;
+        },
+        reset: (): void => {
+            _res = 0;
+        }
+    };
+};
+
+export const createTtiObserver = (): MockObserver => {
+    const timestamps = {
+        start: 0,
+        complete: 0
+    };
+    return {
+        onStart: (): void => {
+            if( timestamps.start === 0 ) {
+                timestamps.start = now();
+            }
+        },
+        onDone: (): void => {
+            // it will keep updating until last done
+            timestamps.complete = now();
+        },
+        getMetrics: (): number => {
+            return timestamps.complete - timestamps.start;
+        },
+        reset: (): void => {
+            timestamps.start = 0;
+            timestamps.complete = 0;
         }
     };
 };
--- packages/profiler/test/observable.test.ts
+++ packages/profiler/test/observable.test.ts
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { createMockObservable, createMockObserver } from '@/mockObservable';
+import { createMockObservable, createMockObserver, createTtiObserver } from '@/mockObservable';
 import { createDebounceObserver } from '@/profiler';
 import { BUSY_INTERVAL } from '@/state';
 import { STATE } from '@/types';
@@ -41,21 +41,31 @@ describe( 'Test observable/observer', () => {
 
 describe( 'Test observable/debounceObserver', () => {
     const output = [] as number[];
+    const tti = [] as number[];
     const mockObservable = createMockObservable();
     const mockObserver = createMockObserver();
+    const ttiObserver = createTtiObserver();
     const debounceObserver = createDebounceObserver( ()=>{
         output.push( mockObserver.getMetrics() );
+        tti.push( ttiObserver.getMetrics() );
+        // do reset here for next cycle
+        mockObserver.reset();
+        ttiObserver.reset();
+        // debounceObserver.reset() --> reset to IDLE?
     } );
 
     beforeEach( () => {
         mockObservable.subscribe( debounceObserver );
         mockObservable.subscribe( mockObserver );
+        mockObservable.subscribe( ttiObserver );
     } );
 
     afterEach( () => {
         output.length = 0;
+        tti.length = 0;
         mockObservable.unsubscribe( debounceObserver );
         mockObservable.unsubscribe( mockObserver );
+        mockObservable.unsubscribe( ttiObserver );
     } );
 
     it( 'Verify debounce observer for one cycle', async() => {
@@ -65,21 +75,33 @@ describe( 'Test observable/debounceObserver', () => {
         await wait( BUSY_INTERVAL );
 
         expect( debounceObserver.getState() ).toEqual( STATE.DONE );
-        expect( mockObserver.getMetrics() ).toEqual( 1 );
+        expect( mockObserver.getMetrics() ).toEqual( 0 );
         expect( output ).toEqual( [ 1 ] );
+        expect( tti[0] ).toBeGreaterThanOrEqual( 0 );
+        expect( tti[0] ).toBeLessThanOrEqual( TOLERANCE );
     } );
 
     it( 'Verify debounce observer for two cycle', async() => {
+        // 1st tick
         mockObservable.mockStart();
         mockObservable.mockDone();
         await wait( BUSY_INTERVAL );
+
+        // 2nd tick
+        mockObservable.mockStart();
+        mockObservable.mockDone();
+        await wait( BUSY_INTERVAL / 2 );
 
         mockObservable.mockStart();
         mockObservable.mockDone();
         await wait( BUSY_INTERVAL );
 
         expect( debounceObserver.getState() ).toEqual( STATE.DONE );
-        expect( mockObserver.getMetrics() ).toEqual( 3 );
-        expect( output ).toEqual( [ 2, 3 ] );
+        expect( mockObserver.getMetrics() ).toEqual( 0 );
+        expect( output ).toEqual( [ 1, 2 ] );
+        expect( tti[0] ).toBeGreaterThanOrEqual( 0 );
+        expect( tti[0] ).toBeLessThanOrEqual( TOLERANCE );
+        expect( tti[1] ).toBeGreaterThanOrEqual( 0 + BUSY_INTERVAL / 2 );
+        expect( tti[1] ).toBeLessThanOrEqual( TOLERANCE + BUSY_INTERVAL / 2 );
     } );
 } );
